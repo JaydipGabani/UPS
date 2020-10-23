5d78b3a8192d5bc1735ecf77083fb6278791e63d
jaydipgabani <gabanijaydip@gmail.com>
2020-10-22 23:36:11 -0400

    sample queries

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -962,6 +962,7 @@ public class Main {
             System.out.println("2. Student");
             System.out.println("3. Employee");
             System.out.println("4. Visitor");
+            System.out.println("5. Run sample queries");
             System.out.println("Any other number to exit: ");
             int u = in.nextInt();
             in.nextLine();
@@ -978,11 +979,62 @@ public class Main {
                 case 4:
                     o.visitorFunction();
                     break;
+                case 5:
+                    o.runSample();
                 default:
                     o.stmt.close();
                     System.exit(0);
                     break;
             }
         }
+    }
+
+    private void runSample() {
+        String s = "select ZONE_DESIGNATION, name from PARKING_LOTS";
+        try {
+            ResultSet rs = this.stmt.executeQuery(s);
+            System.out.println(s);
+            System.out.println();
+            while(rs.next()){
+                System.out.println(rs.getString("ZONE_DESIGNATION") + " " + rs.getString("name"));
+            }
+
+            this.runSampleQuery("select * from PERMIT where PERMIT_ID in (select PERMIT_ID from NON_VISITOR where UNVID = 1006020)");
+
+            this.runSampleQuery("select MODEL, COLOR, YEAR, CAR_MANUFACTURER, VEHICLE_NUMBER from PERMIT where PERMIT_ID in (select PERMIT_ID from NON_VISITOR where UNVID = 1006003)");
+
+            this.runSampleQuery("select SPACE_NUMBER from SPACES where NAME = 'Justice Lot' and DESIGNATED_TYPE = 'electric' and OCCUPIED = 'no'");
+            this.runSampleQuery("select * from CITATION where STATUS = 0");
+            this.runSampleQuery("select count(*) from (select distinct p.PERMIT_ID from PERMIT p, NON_VISITOR n where p.PERMIT_ID = n.PERMIT_ID and p.VEHICLE_NUMBER = n.VEHICLE_NUMBER and zone = 'D')");
+            this.runSampleQuery("select count(*), name from CITATION where CITATION_DATE >= TO_DATE('2020-07-1','YYYY-MM-DD') and CITATION_DATE <= TO_DATE('2020-09-30','YYYY-MM-DD') group by name");
+            this.runSampleQuery("select count(*), p.SPACE_TYPE from PERMIT p, VISITOR v where p.PERMIT_ID = v.PERMIT_ID and p.VEHICLE_NUMBER = v.VEHICLE_NUMBER and p.START_DATE >= TO_DATE('2020-08-12','YYYY-MM-DD') and p.START_DATE <= TO_DATE('2020-08-20','YYYY-MM-DD') GROUP BY p.SPACE_TYPE");
+            this.runSampleQuery("select sum(FEES), CITATION_DATE from CITATION where CITATION_DATE <= TO_DATE('2020-08-31','YYYY-MM-DD') GROUP BY CITATION_DATE");
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void runSampleQuery(String q){try {
+
+
+        System.out.println();
+        System.out.println();
+        String s = q;
+        ResultSet rs = this.stmt.executeQuery(s);
+        ResultSetMetaData rsmd = rs.getMetaData();
+        System.out.println(s);
+        System.out.println();
+        while (rs.next()) {
+            for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                System.out.print(rs.getString(i) + " ");
+            }
+            System.out.println();
+        }
+    } catch (SQLException throwables) {
+        throwables.printStackTrace();
+    }
+
     }
 }
